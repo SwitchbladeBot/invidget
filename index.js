4f5d0cac3ba6e9c5632acf37d4e459354f2f9f22
Pedro Fracassi <pedroluizfracassi@gmail.com>
2019-06-09 13:48:47 -0300

    Add cloudworkers and its script. Refactor code to work with cloudworkers.

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const express = require('express')
-const fetch = require('node-fetch')
-const { repository } = require('./package.json')
-const app = express()
-
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.redirect(repository.url)
+addEventListener ('fetch', event => {
+  event.respondWith(handleRequest(event.request))
 })
 
-app.get('/:inviteCode', async (req, res) => {
-  const inviteInfo = await fetch(`https://discordapp.com/api/v6/invites/${req.params.inviteCode}?with_counts=true`)
-    .then(response => response.json())
-
-  res.status(200).send(inviteInfo)
-})
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
-
-// var svg = require('svg-builder').width()
+async function handleRequest (request) {
+  return new Response("WIP")
+}
