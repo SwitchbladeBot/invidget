654f500a61619b80d129247a344b930ea02aab6d
Pedro Fracassi <pedroluizfracassi@gmail.com>
2019-06-09 14:33:05 -0300

    Fix standard problems

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-addEventListener ('fetch', event => {
+/* eslint-env worker */
+
+addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest (request) {
-  return new Response("WIP")
+  return new Response('WIP')
 }
