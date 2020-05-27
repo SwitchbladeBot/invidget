928dd08e3ec25fcd173090640b8ed90ee5b0b4a9
pedrofracassi <pedroluizfracassi@gmail.com>
2020-05-27 23:01:57 +0000

    Test icon url generation

--- src/Discord.js
+++ src/Discord.js
@@ -5,7 +5,11 @@ module.exports = class Discord {
     return fetch(`https://discordapp.com/api/v6/invites/${inviteCode}?with_counts=true`).then(res => res.json())
   }
 
-  static getIcon (guildId, iconId) {
-    return fetch(`https://cdn.discordapp.com/icons/${guildId}/${iconId}${iconId.startsWith('a_') ? '.gif' : '.jpg'}`).then(res => res.buffer()).then(buffer => buffer.toString('base64'))
+  static fetchIcon (iconUrl) {
+    return fetch(iconUrl).then(res => res.buffer()).then(buffer => buffer.toString('base64'))
+  }
+
+  static getIconUrl(guildId, iconId) {
+    return `https://cdn.discordapp.com/icons/${guildId}/${iconId}${iconId.startsWith('a_') ? '.gif' : '.jpg'}`
   }
 }
