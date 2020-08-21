965dd66377f7ac3c5ca24686d569c21e645136ca
pedrofracassi <pedroluizfracassi@gmail.com>
2020-08-21 15:34:40 +0000

    Fix standard issues

--- src/InviteResolver.js
+++ src/InviteResolver.js
@@ -1,13 +1,13 @@
 const Discord = require('./Discord.js')
 
 const guildIdRegex = /^[0-9]{16,18}$/
-const inviteRegex = /^(?:https?:\/\/)?(?:www\.)?(?:discord(?:app)?)\.(?:com|gg)\/(?:invite\/)?([a-zA-Z0-9\-]+)$/
+const inviteRegex = /^(?:https?:\/\/)?(?:www\.)?(?:discord(?:app)?)\.(?:com|gg)\/(?:invite\/)?([a-zA-Z0-9-]+)$/
 
 module.exports = class InviteResolver {
   static async resolve (query) {
     if (guildIdRegex.test(query)) {
-      const { instant_invite } = await Discord.getWidget(query)
-      return instant_invite.match(inviteRegex)[1]
+      const { instant_invite: inviteUrl } = await Discord.getWidget(query)
+      return inviteUrl.match(inviteRegex)[1]
     }
 
     return query
